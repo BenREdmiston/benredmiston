05c8d583b2ee50ac07d94c612dd46841256feb87
BR3DM <ben.r.edmiston@gmail.com>
2025-04-16 15:08:02 +0800

    updated styles, simplified folders ie one flipcard comp rather than 4 .js files and 4 .css files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import Header from './components/Header';
 import FlipCard from './components/FlipCard';
-import About from './components/sections/About';
-import Contact from './components/sections/Contact.js';
-import Intro from './components/sections/Intro';
-import SkillsPortfolio from './components/sections/SkillsPortfolio';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="container">
-        <FlipCard frontContent="About Me" backContent={<About />} />
-        <FlipCard frontContent="Contact" backContent={<Contact />} />
-        <FlipCard frontContent="Introduction" backContent={<Intro />} />
-        <FlipCard frontContent="Skills/Portfolio" backContent={<SkillsPortfolio />} />
+      <div className='main-layout'>
+        <Header />
+          <div className="container">
+            <FlipCard 
+              frontContent={<h3 className='cardTitle'>Introduction</h3>} 
+              backContent={<p>This is the Introduction section content.</p>} 
+            />
+            <FlipCard 
+              frontContent={<h3 className='cardTitle'>About Me</h3>} 
+              backContent={<p>This is the About Me section content.</p>} 
+            />
+            <FlipCard 
+              frontContent={<h3 className='cardTitle'>Contact</h3>} 
+              backContent={<p>This is the Contact section content.</p>} 
+            />
+            <FlipCard 
+              frontContent={<h3 className='cardTitle'>Skills/Portfolio</h3>} 
+              backContent={<p>This is the Skills/Portfolio section content.</p>} 
+            />
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,12 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header-title">Benjamin Edmiston</h1>
+      <img src="https://avatars.githubusercontent.com/u/77869567?s=400&u=000e963c53758701e07014fa9bb810146f5887c7&v=4" alt="Benjamin Edmiston" className="header-avatar" />
+      <ul className="header-description">
+        <li> Web Designer </li>
+        <li> Poo </li>
+        <li> Wee </li>
+      </ul>
     </header>
   );
 }
